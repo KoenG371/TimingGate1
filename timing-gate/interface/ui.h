c72efd2c1fec271490cd1b47b9447945fa867c00
KoenG371 <87037268+KoenG371@users.noreply.github.com>
2022-12-10 10:43:19 +0100

    Add files via upload

---
+++ timing-gate/interface/ui.h
@@ -0,0 +1,88 @@
+// SquareLine LVGL GENERATED FILE
+// EDITOR VERSION: SquareLine Studio 1.0.5
+// LVGL VERSION: 8.2
+// PROJECT: Timinggate_test
+
+#ifndef _TIMINGGATE_TEST_UI_H
+#define _TIMINGGATE_TEST_UI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if __has_include("lvgl.h")
+#include "lvgl.h"
+#else
+#include "lvgl/lvgl.h"
+#endif
+
+extern lv_obj_t * ui_Screen1;
+extern lv_obj_t * ui_Label2;
+extern lv_obj_t * ui_background;
+extern lv_obj_t * ui_backgroundcolor2;
+extern lv_obj_t * ui_backgroundcolor;
+extern lv_obj_t * ui_startbutton;
+extern lv_obj_t * ui_Label1;
+extern lv_obj_t * ui_offbutton;
+extern lv_obj_t * ui_menubutton;
+extern lv_obj_t * ui_lastruntotal;
+extern lv_obj_t * ui_lastrunsector1;
+extern lv_obj_t * ui_lastrunsector2;
+extern lv_obj_t * ui_lastrunsector3;
+extern lv_obj_t * ui_Label3;
+extern lv_obj_t * ui_Label4;
+extern lv_obj_t * ui_Label5;
+extern lv_obj_t * ui_Label6;
+extern lv_obj_t * ui_Label7;
+extern lv_obj_t * ui_Label8;
+extern lv_obj_t * ui_Label9;
+extern lv_obj_t * ui_Label10;
+extern lv_obj_t * ui_Label11;
+extern lv_obj_t * ui_Label12;
+extern lv_obj_t * ui_Label13;
+extern lv_obj_t * ui_Label14;
+extern lv_obj_t * ui_Label15;
+extern lv_obj_t * ui_t1sector1;
+extern lv_obj_t * ui_t1sector2;
+extern lv_obj_t * ui_t1sector3;
+extern lv_obj_t * ui_t2sector3;
+extern lv_obj_t * ui_t2sector2;
+extern lv_obj_t * ui_t2sector1;
+extern lv_obj_t * ui_t3sector1;
+extern lv_obj_t * ui_t3sector2;
+extern lv_obj_t * ui_t3sector3;
+extern lv_obj_t * ui_Label25;
+extern lv_obj_t * ui_Label26;
+extern lv_obj_t * ui_t1total;
+extern lv_obj_t * ui_Label28;
+extern lv_obj_t * ui_Label29;
+extern lv_obj_t * ui_t2total;
+extern lv_obj_t * ui_t3total;
+extern lv_obj_t * ui_showhistory;
+extern lv_obj_t * ui_Label32;
+extern lv_obj_t * ui_Label33;
+extern lv_obj_t * ui_Label34;
+extern lv_obj_t * ui_Screen3;
+extern lv_obj_t * ui_backgroundscreen3;
+extern lv_obj_t * ui_Label35;
+extern lv_obj_t * ui_Label36;
+extern lv_obj_t * ui_Menu4;
+extern lv_obj_t * ui_Label37;
+extern lv_obj_t * ui_Label38;
+
+void Start_button(lv_event_t * e);
+void Start_button(lv_event_t * e);
+void drop_gate(lv_event_t * e);
+void Start_button(lv_event_t * e);
+
+
+
+
+
+void ui_init(void);
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
---
+++ timing-gate/interface/ui_events.c
@@ -0,0 +1,16 @@
+// SquareLine LVGL GENERATED FILE
+// EDITOR VERSION: SquareLine Studio 1.0.5
+// LVGL VERSION: 8.2
+// PROJECT: Timinggate_test
+
+#include "ui.h"
+
+void Start_button(lv_event_t * e)
+{
+	// Your code here
+}
+
+void drop_gate(lv_event_t * e)
+{
+	// Your code here
+}
